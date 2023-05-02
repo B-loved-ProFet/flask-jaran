97ac9b724b90c984a3a0997ae731872ca56f6811
Szymi13 <urbanszymon13@gmail/com>
2023-05-02 03:08:38 +0200

    start testing emails

--- app.py
+++ app.py
@@ -1,5 +1,21 @@
 from flask import Flask
+from flask_mailman import Mail, EmailMessage
+
 app = Flask(__name__)
+
+mail = Mail(app)
+connection = Mail.get_connection()
+connection.open()
+
+email1 = EmailMessage(
+    'Hello',
+    'Body goes here',
+    'lewyywel@gmail.com',
+    ['urbanszymon13@gmail.com'],
+    connection=connection,
+)
+
+email1.send()
 
 @app.route('/')
 def hello_world():
